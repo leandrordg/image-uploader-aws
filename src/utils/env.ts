6e45f7e7aef50194d4720febd8a5c5bc4ab020df
Leandro Rodrigues <lj251004@gmail.com>
2025-03-15 13:43:53 -0300

    fix: remove unused environment variables from env schema

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -4,8 +4,6 @@ const envSchema = z.object({
   POSTGRES_URL: z.string(),
   AUTH_GOOGLE_ID: z.string(),
   AUTH_GOOGLE_SECRET: z.string(),
-  AUTH_RESEND_KEY: z.string(),
-  AUTH_RESEND_FROM: z.string(),
   AUTH_SECRET: z.string(),
 });
 
